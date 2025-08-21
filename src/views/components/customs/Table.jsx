1e8674cb6c0cd1f9a83f54380311f87fb71c0521
Clement-Zhang <zhangclement947@yahoo.ca>
2025-08-20 21:49:59 -0400

    switch devices properly

---
+++ src/views/components/customs/Table.jsx
@@ -0,0 +1,8 @@
+import Header from './customs/Header';
+import Form from './customs/Form';
+import Image from './customs/Image';
+import editIcon from '../../assets/icons/edit.png';
+import remove from '../../assets/icons/remove.png';
+import { editError } from '../../../configs/form/error';
+import { useData } from '../../contexts/data';
+import { useModal } from '../../contexts/modal';
